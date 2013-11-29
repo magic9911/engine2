55279bc747ad39a01f7c0cf66c852c6a1bf026fc
Iran <ma1kelvdh@gmail.com>
2013-11-29 14:06:35 +0100

    added some WIP auto-ss code for the future, disabled atm

--- src/code.asm
+++ src/code.asm
@@ -11,6 +11,7 @@ retn ; For some reason the game executes the first function found in the extende
 %include "src/spawner/tunnel.asm"
 %include "src/spawner/nethack.asm"
 %include "src/spawner/selectable_spawns.asm"
+;%include "src/spawner/auto_ss.asm"
 
 ; other
 %include "src/display_messages_typed_by_yourself.asm"
---
+++ src/spawner/auto_ss.asm
@@ -0,0 +1,20 @@
+@JMP 0x00509383 _Main_Loop_Auto_SS
+
+_Main_Loop_Auto_SS:
+    mov edx, 0
+    mov ebx, 255
+    mov eax, [0x007E4924] ; Frame
+    
+    idiv ebx
+    
+    cmp dx, 1
+    jnz .Ret
+
+    call 0x004EAB00 ; screen capture
+    
+    
+
+
+.Ret:
+    call 0x005094A0; Sync_Delay(void)
+    jmp 0x00509388
