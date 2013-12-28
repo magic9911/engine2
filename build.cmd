77367d35c7ae8634152a8a00922cad5dfdc0d26a
Toni Spets <toni.spets@iki.fi>
2013-12-28 19:54:52 +0200

    VirtualSize hack for .data

--- build.cmd
+++ build.cmd
@@ -1,6 +1,7 @@
 tools\nasm.exe -f elf src\patch.asm -o patch.o
-tools\ld.exe -T gamemd.lds --subsystem=windows -o gamemd.exe
+tools\ld.exe -T gamemd.lds --file-alignment=4096 --subsystem=windows -o gamemd.exe
 tools\petool.exe patch gamemd.exe
 tools\petool.exe setdd gamemd.exe 1 0x40F0E0 320
+tools\petool.exe setvs gamemd.exe .data 3570660
 tools\strip.exe -R .patch gamemd.exe
 tools\petool.exe dump gamemd.exe 
