31c163265a0542cb3770c78896ff85e51af973e6
Toni Spets <toni.spets@iki.fi>
2013-12-28 20:04:02 +0200

    Fix up commented out source

--- src/no-cd.asm
+++ src/no-cd.asm
@@ -1,13 +1,15 @@
 ; Info taken from Ares source code
 ; https://github.com/Ares-Developers/Ares/blob/master/src/Misc/CopyProtection.cpp
 
-@JMP 0x004A80D0 _CD_AlwaysFindYR
-@JMP 0x004790E0 _CD_AlwaysAvailable
-@JMP 0x00479110 _CD_NeverAsk
-@JMP 0x0052C3B3 _Init_Game_NoCD_Check
-@JMP 0x00531236 _Init_Secondary_MixFiles_Continue_When_Movies_Missing
-@JMP 0x006BE719 _WinMain_Fix_Crash_When_NoCD_Enabled
+section .patch
+@JMP 0x004A80D0, _CD_AlwaysFindYR
+@JMP 0x004790E0, _CD_AlwaysAvailable
+@JMP 0x00479110, _CD_NeverAsk
+@JMP 0x0052C3B3, _Init_Game_NoCD_Check
+@JMP 0x00531236, _Init_Secondary_MixFiles_Continue_When_Movies_Missing
+@JMP 0x006BE719, _WinMain_Fix_Crash_When_NoCD_Enabled
 
+section .text
 ; add NULL pointer check
 _WinMain_Fix_Crash_When_NoCD_Enabled:
     mov ecx, dword [0x00884E2C] ; MoviesMix?
