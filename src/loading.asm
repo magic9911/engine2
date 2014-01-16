45c06cfd1c452444503f5b8735e6787a7c4ad748
John Ericson <Ericson2314@Yahoo.com>
2014-01-16 04:00:12 -0500

    Simplified loading.asm

--- src/loading.asm
+++ src/loading.asm
@@ -11,25 +11,19 @@ extern WindowedMode
 section .text
 
 _WinMain_Read_RA2md_INI_Read_Windowed_Option:
+; instructions which jump clobbered
     call INIClass__GetBool
     mov byte [0x840A6C], al
-    
+
 ; Start loading our own settings:
 
     call_INIClass__GetBool INIClass_RA2md_INI, str_Options, str_NoCD, 1
-    cmp al, 0
-    jz .Dont_Set_NoCD
     mov byte [IsNoCD], al
-.Dont_Set_NoCD: 
- 
+
     call_INIClass__GetBool INIClass_RA2md_INI, str_Video, str_UseGraphicsPatch, 1
     mov byte [UseGraphicsPatch], al
-    
+
     call_INIClass__GetBool INIClass_RA2md_INI, str_Video, str_Windowed, 0
-    cmp al, 0
-    jz  .Dont_Set_Windowed_Mode
-    mov byte [WindowedMode], 1
-    
-.Dont_Set_Windowed_Mode:  
-    
+    mov byte [WindowedMode], al
+
     jmp 0x006BC0E6
