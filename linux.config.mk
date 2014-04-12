aa2b90179ca73c20fab590dffb8752c9292b7f2d
John Ericson <Ericson2314@Yahoo.com>
2014-04-12 10:41:40 -0700

    PeTool assumed to be on path, linux.config.mk included

---
+++ linux.config.mk
@@ -0,0 +1,18 @@
+# This config should make all the tweaks nessisary to build on linux
+
+WINDRES     = i686-w64-mingw32-windres
+LD          = i686-w64-mingw32-ld
+
+# should be tools repo
+PETOOL     ?= ../petool/petool$(EXT)
+
+# needed because config gets included first
+build_both_games: default
+
+$(PETOOL):
+	$(MAKE) -C $(dir $(PETOOL)) EXT=$(EXT)
+
+clean_tools:
+	$(MAKE) -C $(dir $(PETOOL)) EXT=$(EXT) clean
+
+.PHONY: build_both_games clean_tools
