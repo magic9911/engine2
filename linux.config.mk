54f122d5157a662e684a1d2aa6fe5409531141b3
John Ericson <Ericson2314@Yahoo.com>
2014-04-13 14:40:38 -0700

    linux.config.mk shouldn't have ?=

--- linux.config.mk
+++ linux.config.mk
@@ -4,7 +4,7 @@ WINDRES     = i686-w64-mingw32-windres
 LD          = i686-w64-mingw32-ld
 
 # should be tools repo
-PETOOL     ?= ../petool/petool$(EXT)
+PETOOL      = ../petool/petool$(EXT)
 
 # needed because config gets included first
 build_both_games: default
