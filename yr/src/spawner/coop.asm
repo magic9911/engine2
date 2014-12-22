ce5d50891d0157404f5ee7523dc654c299e016c9
iran.laptop <ma1kelvdh@gmail.com>
2014-12-22 20:08:03 +0100

    add hack to prevent crash while loading coop map, something related to failing coop map file name detection and loadign screen stuff, loading screen text?

---
+++ yr/src/spawner/coop.asm
@@ -0,0 +1,5 @@
+%include "macros/patch.inc"
+%include "macros/hack.inc"
+%include "macros/string.inc"
+
+@LJMP 0x00553321, 0x005533C5 ; skip some stuff detecting coop map for loading screen stuff (loading screen text?)
