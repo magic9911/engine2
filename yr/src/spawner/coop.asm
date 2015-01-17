95a92fd9f0699936c068e6c337ab405d4153d8c1
iran.laptop <ma1kelvdh@gmail.com>
2015-01-17 22:17:17 +0100

    fix coop endgame crash

--- yr/src/spawner/coop.asm
+++ yr/src/spawner/coop.asm
@@ -3,3 +3,9 @@
 %include "macros/string.inc"
 
 @LJMP 0x00553321, 0x005533C5 ; skip some stuff detecting coop map for loading screen stuff (loading screen text?)
+
+@LJMP 0x0055D0E2, _Coop_Endgame_Crash_Fix
+
+_Coop_Endgame_Crash_Fix:
+	add esp,4 ; skip original function call
+	jmp 0x0055D0E8
