912a52c51677202a0252d6c61dc30772bc70f03d
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2015-01-21 04:27:08 +0100

    remove outdated files

--- ts/src/fix_mouse_not_found_error.asm
+++
@@ -1,6 +0,0 @@
-%include "macros/patch.inc"
-%include "macros/hack.inc"
-%include "macros/string.inc"
-
-;"Tiberian Sun requires a mouse to play" error - remove the GetSystemMetrics check
-@CLEAR 0x005FFDCC, 0x90, 0x005FFDEA
--- ts/src/no_options_menu_animation.asm
+++
@@ -1,5 +0,0 @@
-%include "macros/patch.inc"
-
-; Disable the options menu opening animation
-@LJMP 0x00593EDD, 0x005942CC
-@LJMP 0x00593EE5, 0x005942CC
